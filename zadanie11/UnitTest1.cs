41d530c319301e7f6f0122b0f02fbd0788f9b602
Irina <48284966+leontevaira9812@users.noreply.github.com>
2019-05-13 19:47:49 +0300

    change email

--- zadanie11/UnitTest1.cs
+++ zadanie11/UnitTest1.cs
@@ -26,8 +26,11 @@ public class MyFirstTest
     [Test]
     public void FirstTest()
     {
-      
-        driver.Url = "http://localhost/litecart/en/";
+            string s = "";
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+                s += (char)(r.Next('a', 'z'));
+            driver.Url = "http://localhost/litecart/en/";
             driver.FindElement(By.LinkText("New customers click here")).Click();
             driver.FindElement(By.Name("firstname")).SendKeys("Ira");
             driver.FindElement(By.Name("lastname")).SendKeys("leo");
@@ -36,7 +39,7 @@ public class MyFirstTest
             driver.FindElement(By.Name("city")).SendKeys("abc");
            
             SelectElement objSelect = new SelectElement(driver.FindElement(By.Name("country_code")));
-            driver.FindElement(By.Name("email")).SendKeys("leoonttevaira95@gmail.com"); 
+            driver.FindElement(By.Name("email")).SendKeys(s+"@gmail.com"); 
             objSelect.SelectByText("United States");
             driver.FindElement(By.Name("phone")).SendKeys("+12345678900");
             driver.FindElement(By.Name("password")).SendKeys("1234");
@@ -44,7 +47,7 @@ public class MyFirstTest
         
             driver.FindElement(By.Name("create_account")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
-            driver.FindElement(By.Name("email")).SendKeys("leoonttevaira95@gmail.com");
+            driver.FindElement(By.Name("email")).SendKeys(s+"@gmail.com");
             driver.FindElement(By.Name("password")).SendKeys("1234");
             driver.FindElement(By.Name("login")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
